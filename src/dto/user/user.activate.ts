bcd979cb9747f212ff190d8fb881825bf31239e3
bastiaanv <bastiankpn7800@gmail.com>
2020-03-12 09:52:35 +0100

    Update regex to accept special characters

--- src/dto/user/user.activate.ts
+++ src/dto/user/user.activate.ts
@@ -4,7 +4,7 @@ import { IsNotEmpty, IsUUID, Matches } from 'class-validator';
 export class UserActivateDTO {
     // Min 8 char long, 1 lowercase, 1 uppercase and 1 number
     @IsNotEmpty()
-    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/)
+    @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\w\d_@.\#\$&+-]{8,}$/)
     @ApiProperty({type: String, format: 'password'})
     public password: string;
 
