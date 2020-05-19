617e19e25ea3abb4361c4d0a2d3486242e4c6794
Bastiaan Verhaar <bastiankpn7800@gmail.com>
2020-05-19 17:26:47 +0200

    Removed Cron job

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,8 +10,6 @@ import { APP_GUARD } from '@nestjs/core';
 import { AuthorizationGuard } from './decorators/auth.decorator';
 import { StatisticRepository } from './repositories/statistic.repository';
 import { StatisticsController } from './controllers/statistics/statistics.controller';
-import { ScheduleModule } from '@nestjs/schedule';
-import { StatisticsJob } from './jobs/statistics.job';
 import { BoardController } from './controllers/board/board.controller';
 import { BoardRepository } from './repositories/board.repository';
 import { EmailService } from './services/email/email.service';
@@ -19,7 +17,6 @@ import { EmailService } from './services/email/email.service';
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeormConfig as TypeOrmModuleOptions),
-    ScheduleModule.forRoot()
   ],
   controllers: [
     AgendaController,
@@ -28,7 +25,6 @@ import { EmailService } from './services/email/email.service';
     BoardController,
   ],
   providers: [
-    StatisticsJob,
     {
       provide: APP_GUARD,
       useClass: AuthorizationGuard,
