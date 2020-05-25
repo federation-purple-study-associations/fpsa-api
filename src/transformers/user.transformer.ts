dea2e8ed8dc168eee9bcf77101bde1d40e328358
Bastiaan Verhaar <3987804+bastiaanv@users.noreply.github.com>
2020-05-25 12:06:23 +0200

    Fix user -> name transform

--- src/transformers/user.transformer.ts
+++ src/transformers/user.transformer.ts
@@ -91,7 +91,7 @@ export class UserTransformer {
     public static toApplication(body: NewApplication) {
         const application = new Application();
         application.email = body.email;
-        application.name = UserTransformer.checkName(body.name);
+        application.name = body.name;
         application.academy = body.academy;
         application.kvk = body.kvk;
         application.establishment = body.establishment;
@@ -99,19 +99,4 @@ export class UserTransformer {
         return application;
     }
 
-    /**
-     * This function will make sure that the name of a study association will be forced to: s.v. {NAME}, if it isn't
-     * @param name Name of study association
-     */
-    private static checkName(name: string): string {
-        if (name.substr(0, 5) === 's.v. ') {
-            return name;
-        
-        } else if (name.substr(0, 3) === 'sv ' || name.substr(0, 4) === 's.v ' || name.substr(0, 4) === 'sv. ') {
-            return 's.v. ' + name.substring(3);
-        
-        } else {
-            return 's.v. ' + name;
-        }
-    }
 }
