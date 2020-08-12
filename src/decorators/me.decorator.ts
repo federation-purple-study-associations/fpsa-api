8957b878b75a10ba9cef5bedfbd2fb6ed5237735
Bastiaan Verhaar <bastiankpn7800@gmail.com>
2020-08-12 17:45:27 +0200

    Fix me decorator?

--- src/decorators/me.decorator.ts
+++ src/decorators/me.decorator.ts
@@ -1,10 +1,11 @@
-import { createParamDecorator, UnauthorizedException } from "@nestjs/common";
+import { createParamDecorator, UnauthorizedException, ExecutionContext } from "@nestjs/common";
 import { User } from '../entities/user/user.entity';
 import { UserTransformer } from '../transformers/user.transformer';
 
 const errorNoAuthCookieFound = 'No authorization cookie has been found... Please make sure that you are logged in before performing this action';
 
-export const Me = createParamDecorator(async (data: any, request: any): Promise<User> => {
+export const Me = createParamDecorator(async (data: string, ctx: ExecutionContext): Promise<User> => {
+    const request = ctx.switchToHttp().getRequest();
     if (!request.headers.cookie) {
         throw new UnauthorizedException(errorNoAuthCookieFound);
     }
